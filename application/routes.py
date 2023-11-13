50f0c5e39f5cbe1204bd613ef9194ed18b1ac6a1
Xingguo Huang <76279571+308241561@users.noreply.github.com>
2023-11-12 22:42:22 -0800

    URL variables

--- application/routes.py
+++ application/routes.py
@@ -7,15 +7,16 @@ from flask import render_template
 def index():
     return render_template("index.html", index = True)
 
-@app.route('/login')
+@app.route("/login")
 def login():
     return render_template("login.html", login = True)
 
-@app.route('/courses')
-def courses():
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term = "Spring 2019"):
     courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
     
-    return render_template("courses.html", courseData = courseData, courses = True)
+    return render_template("courses.html", courseData = courseData, courses = True, term = term)
 
 @app.route('/register')
 def register():
